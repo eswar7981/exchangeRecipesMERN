ea01538c60e16b6a4749d15944edc265ae2cdc8f
Sai Eswar <126505498+eswar7981@users.noreply.github.com>
2024-07-15 16:41:50 +0530

    Update databasePGSql.js

--- backend/database/databasePGSql.js
+++ backend/database/databasePGSql.js
@@ -2,13 +2,9 @@ const { HostAddress } = require('mongodb')
 const {Client}=require('pg')
 
 require('dotenv').config()
-
+const connectionString =`postgresql://postgres1:wtmlHswUdAp1LVWEAkPLyWsRkjAkZCT6@dpg-cqad3ftds78s739pou20-a/exchangerecipes`
 const client=new Client({
-    host:process.env.LOCALHOST,
-    user:process.env.USER,
-    port:process.env.PORT,
-    password:`@Eswar158`,
-    database:process.env.DATABASE
+    connectionString:connectionString
 
 })
 
